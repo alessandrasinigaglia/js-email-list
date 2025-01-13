bc2607376a87972459063ea5c4e2e949132e6fdd
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-13 22:05:13 +0100

    finish project

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-//Dom ELM
+//DOM ELM
 const emailList = document.getElementById('email');
 const generateEmails = document.getElementById('gen');
 
+//FUNCTION
+function generateEmail() {
+
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(function (response) {
+            console.log(response.data.response);
+        emailList.innerHTML += `<li>${response.data.response}</li>`
+        })
+    }
+
+}
+
+generateEmail()
+
+//EVENTS
+generateEmails.addEventListener('click', generateEmail)
